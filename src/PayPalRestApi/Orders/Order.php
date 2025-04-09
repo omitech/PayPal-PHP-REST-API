b18189a2ab272c69f3d06c55507186ad47a30960
omitech <omitech@users.noreply.github.com>
2025-04-09 17:48:05 +0200

    Order fix responses

--- src/PayPalRestApi/Orders/Order.php
+++ src/PayPalRestApi/Orders/Order.php
@@ -16,16 +16,7 @@ class Order
     // Method to create the order
     public function create(OrderData $data): string|void
     {
-        $response = $this->client->request('POST', '/v2/checkout/orders', $data->toPayPalFormat());
-
-        // Find the approval URL from the response
-        foreach ($data['links'] as $link) {
-            if ($link['rel'] == 'approve') {
-                return $link->href; // Return the approval URL
-            }
-        }
-
-        return null;
+        return $this->client->request('POST', '/v2/checkout/orders', $data->toPayPalFormat());
     }
 
     // Method to capture the order
@@ -33,8 +24,8 @@ class Order
     {
         $response = $this->client->request('POST', "/v2/checkout/orders/{$orderId}/capture");
 
-        if ($data['status'] == 'COMPLETED') {
-            return $data; // Return the successful capture response
+        if ($response['status'] == 'COMPLETED') {
+            return $response; // Return the successful capture response
         } else {
             return null; // Handle failure cases
         }        
